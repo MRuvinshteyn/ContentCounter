e3a7e3658a5ffafcfd75902edb443121353597ba
Michael Ruvinshteyn <michael.ruvinshteyn@gmail.com>
2023-09-04 16:25:44 -0400

    add millisecond counter for all tracks

--- ContentCounter/EntryPoint.cs
+++ ContentCounter/EntryPoint.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ScriptPortal.Vegas;
+using System.Windows.Forms;
 
 namespace ContentCounter
 {
     public class EntryPoint
     {
+        public void FromVegas(Vegas vegas)
+        {
+            foreach (Track track in vegas.Project.Tracks)
+            {
+                double totalLength = 0;
+                foreach (TrackEvent trackEvent in track.Events)
+                {
+                    totalLength += trackEvent.Length.ToMilliseconds();
+                }
+                MessageBox.Show(totalLength.ToString());
+            }
+        }
     }
 }
