036bfdb655cd74879bb4a469d7de3033a98b4dc4
Michael Ruvinshteyn <michael.ruvinshteyn@gmail.com>
2023-09-05 02:19:28 -0400

    create empty prompt for selecting tracks

--- ContentCounter/EntryPoint.cs
+++ ContentCounter/EntryPoint.cs
@@ -1,4 +1,6 @@
-﻿using ScriptPortal.Vegas;
+﻿using ContentCounter.GUI;
+using ScriptPortal.Vegas;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace ContentCounter
@@ -7,23 +9,26 @@ namespace ContentCounter
     {
         public void FromVegas(Vegas vegas)
         {
-            foreach (Track track in vegas.Project.Tracks)
-            {
-                Timecode totalLength = new Timecode();
-                foreach (TrackEvent trackEvent in track.Events)
-                {
-                    if (trackEvent.Start < vegas.SelectionStart)
-                    {
-                        continue;
-                    }
-                    if (trackEvent.Start + trackEvent.Length > vegas.SelectionStart + vegas.SelectionLength)
-                    {
-                        break;
-                    }
-                    totalLength += trackEvent.Length;
-                }
-                MessageBox.Show(totalLength.ToString());
-            }
+            MessageBox.Show(vegas.Project.Tracks.Count.ToString());
+            TrackSelectionPrompt prompt = new TrackSelectionPrompt(vegas.Project.Tracks.Select(track => track.Name));
+            prompt.Show();
+            //foreach (Track track in vegas.Project.Tracks)
+            //{
+            //    Timecode totalLength = new Timecode();
+            //    foreach (TrackEvent trackEvent in track.Events)
+            //    {
+            //        if (trackEvent.Start < vegas.SelectionStart)
+            //        {
+            //            continue;
+            //        }
+            //        if (trackEvent.Start + trackEvent.Length > vegas.SelectionStart + vegas.SelectionLength)
+            //        {
+            //            break;
+            //        }
+            //        totalLength += trackEvent.Length;
+            //    }
+            //    MessageBox.Show(totalLength.ToString());
+            //}
         }
     }
 }
---
+++ ContentCounter/GUI/TrackSelectionPrompt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContentCounter.GUI
+{
+    public partial class TrackSelectionPrompt : Form
+    {
+        public TrackSelectionPrompt()
+        {
+            InitializeComponent();
+        }
+
+        public TrackSelectionPrompt(IEnumerable<string> tracks)
+        {
+            InitializeComponent();
+        }
+    }
+}
